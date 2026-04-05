82dadc2be7d1abbdee72ea2665bf852b64b21500
Andres Camilo Alvarez Montoya <dev@acalmo.me>
2026-04-05 15:17:58 -0500

    feat: implement Gemini chat service and configuration management

--- app/services/chat_gemini.py
+++ app/services/chat_gemini.py
@@ -1,11 +1,12 @@
 from google import genai
 from app.core.config import config
 
-client = genai.Client(api_key=config.google_api_key)
+client = genai.Client(vertexai=True)
 
 
 def get_chat_stream(prompt: str):
     return client.models.generate_content_stream(
         model=config.model,
         contents=prompt,
+        config=config.content_config,
     )
